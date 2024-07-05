ce0e126d111a458746d2fbd6e6c9d5fb3938b323
seoyeonpp <seoyeonpub@gmail.com>
2024-07-05 16:32:44 +0900

    api 방식 실습

--- src/main/java/com/example/hello/controller/HelloController.java
+++ src/main/java/com/example/hello/controller/HelloController.java
@@ -5,6 +5,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @RequestMapping("/hello")
 
@@ -21,5 +22,32 @@ public class HelloController {
     public String helloMvc(@RequestParam(value = "name", defaultValue = "") String name, Model model) {
         model.addAttribute("name",name);
         return "/hello-template";
+    }
+
+    @GetMapping("/string")
+    @ResponseBody //http에서 body 부분에 이 내용을 직접 넣어주겠다.
+    public String helloString(@RequestParam("name") String name) {
+        return "hello " + name;
+    }
+
+//    api방식은 객체를 반환하는것 / view 없이 http response에다가 값을 딱 넣어주는것
+    @GetMapping("/api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    static class Hello {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
