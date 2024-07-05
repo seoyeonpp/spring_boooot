15e160c199d05021b9ba42dafa01471d44892fb3
seoyeonpp <seoyeonpub@gmail.com>
2024-07-05 15:47:58 +0900

    requestMapping 추가

--- src/main/java/com/example/hello/controller/HelloController.java
+++ src/main/java/com/example/hello/controller/HelloController.java
@@ -3,12 +3,23 @@ package com.example.hello.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@RequestMapping("/hello")
 
 @Controller
 public class HelloController {
-    @GetMapping("/hello")
+
+    @GetMapping("")
     public String hello(Model model) {
         model.addAttribute("data", "hello!!");
         return "/hello";
+    }
+
+    @GetMapping("/mvc")
+    public String helloMvc(@RequestParam(value = "name", defaultValue = "") String name, Model model) {
+        model.addAttribute("name",name);
+        return "/hello-template";
     }
 }
