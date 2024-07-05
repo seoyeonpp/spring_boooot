939e7859fe7c8c1477ad6d0c1bc8afa7932777e6
seoyeonpp <seoyeonpub@gmail.com>
2024-07-05 09:48:31 +0900

    수정

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -1,18 +1,18 @@
 package com.example.demo.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.util.StopWatch;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Date;
 
-@RestController
-public class DemoController {
-
+@Controller
+public class HelloController {
     @GetMapping("/hello")
-    public String hello() {
-
-        return "hello world !!";
+    public String hello(Model model) {
+        model.addAttribute("data", "hello!!");
+        return "/hello";
     }
-
 }
