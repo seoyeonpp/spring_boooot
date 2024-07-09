bdff8dd7b53f67017276208f67c73fdfb953e974
seoyeonpp <seoyeonpub@gmail.com>
2024-07-09 15:21:41 +0900

    회원조회 기능 추가

--- src/main/java/com/example/hello/controller/MemberController.java
+++ src/main/java/com/example/hello/controller/MemberController.java
@@ -4,8 +4,11 @@ import com.example.hello.domain.Member;
 import com.example.hello.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -52,6 +55,16 @@ public class MemberController {
         memberService.join(member);
 
         return "redirect:/";
+    }
+
+    /**
+     * 회원목록 조회
+     */
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members",members);
+        return "/members/memberList";
     }
 
 }
