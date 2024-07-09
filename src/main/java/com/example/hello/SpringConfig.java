654f72c34d5ee8e4fee2da2378ddc4f9d90da356
seoyeonpp <seoyeonpub@gmail.com>
2024-07-09 16:53:23 +0900

    jdbc 고대기술 Jdbc 리포지토리 구현 ~

--- src/main/java/com/example/hello/SpringConfig.java
+++ src/main/java/com/example/hello/SpringConfig.java
@@ -1,10 +1,13 @@
 package com.example.hello;
 
+import com.example.hello.repository.JdbcMemberRepository;
 import com.example.hello.repository.MemberRepository;
 import com.example.hello.repository.MemoryMemberRepository;
 import com.example.hello.service.MemberService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
 
 
 // @Autowired 안쓰고 자바 코드로 직접 스프링 빈 등록하는 방법
@@ -15,8 +18,14 @@ public class SpringConfig {
         return new MemberService(memberRepository());
     }
 
+    private final DataSource dataSource;
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+//        return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
