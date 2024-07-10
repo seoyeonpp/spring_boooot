42a558fe8aa2f0ef4ef95d814bb2957a38757ba9
seoyeonpp <seoyeonpub@gmail.com>
2024-07-10 09:01:04 +0900

    스프링 통합 테스트 실습

--- src/test/java/com/example/hello/service/MemberServiceIntegrationTest.java
+++ src/test/java/com/example/hello/service/MemberServiceIntegrationTest.java
@@ -1,37 +1,33 @@
 package com.example.hello.service;
 
 import com.example.hello.domain.Member;
+import com.example.hello.repository.MemberRepository;
 import com.example.hello.repository.MemoryMemberRepository;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
+import org.springframework.transaction.annotation.Transactional;
 
-import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
-// 순수한 단위 테스트 - 단위 테스트를 잘 만드는게 훨씬 좋다.
-class MemberServiceTest {
+// 스프링 통합 테스트 - DB까지 연결해서 테스트
+@SpringBootTest // 스프링 컨테이너와 함께 테스트를 실행한다.
+@Transactional // 테스트케이스에 이 어노테이션이 있으면 테스트 시작 전에 트랜잭션을 시작하고 테스트가 끝나면 롤백을 해줌.
+public class MemberServiceIntegrationTest {
 
-    MemberService memberService;
-    MemoryMemberRepository memberRepository;
-
-    @BeforeEach
-    public void beforeEach() {
-        memberRepository = new MemoryMemberRepository();
-        memberService = new MemberService(memberRepository);
-    }
-
-    @AfterEach
-    public void afterEach() {
-        memberRepository.clearStore();
-    }
+    @Autowired MemberService memberService;
+    @Autowired MemberRepository memberRepository;
 
     @Test
+    // @Commit - 테스트케이스에 이 어노테이션이 있으면 테스트가 끝나고 나서도 롤백을 하지 않고 커밋을 해줌.
     void join() {
         // given
         Member member = new Member();
-        member.setName("hello");
+        member.setName("spring1");
 
         // when
         Long saveId = memberService.join(member);
@@ -65,11 +61,4 @@ class MemberServiceTest {
         // then
     }
 
-    @Test
-    void findMembers() {
-    }
-
-    @Test
-    void findOne() {
-    }
 }
